7a8b658f2db93065b3e77183b0ac13ea28095412
Bruno mendonça <bruno.mendonca@digitaly.tech>
2025-03-28 15:13:28 -0300

    comentarios de rotas

--- src/routers/routerCliente.js
+++ src/routers/routerCliente.js
@@ -3,15 +3,19 @@ import ctrlCliente from "../controllers/ctrlCliente.js";
 
 const routerCliente = Router()
 
+//GET
 routerCliente.get('/clientes', ctrlCliente.index)
 routerCliente.get('/clientes/:telefone', ctrlCliente.findByPhone)
 routerCliente.get('/clientes/status/:status', ctrlCliente.getByStatus)
 
+//POST
 routerCliente.post('/clientes', ctrlCliente.create)
 routerCliente.post('/clientes/enabledisable/:id', ctrlCliente.disableOrEnable)
 
+//DELETE
 routerCliente.delete('/clientes/:id', ctrlCliente.delete)
 
+//PUT
 routerCliente.put('/clientes/:id', ctrlCliente.update)
 
 export default routerCliente
